61a1056f09d947de87fbe8ea92a0df54a2786d3b
ellikamishra <ellikamishra009@gmail.com>
2022-04-03 17:30:14 +0530

    web3 js library and home page code added

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './Components/HomePage/HomePage';'./Components/HomePage'
+function App() {
+  return (
+    <HomePage/>
+  );
+}
+
+export default App;
---
+++ src/Components/HomePage/HomePage.jsx
@@ -0,0 +1,50 @@
+import React from "react"
+import "./styles.css"
+
+export default function HomePage() {
+  return (
+    <div className="home flex-col-hstart-vstart clip-contents">
+      <div className="rectangle-1" />
+      <div className="group-9510 flex-row-vend">
+        <div className="frame-30 flex-row-vcenter-hcenter">
+          <p className="txt-1069">HirePlus</p>
+          <div className="frame-1 flex-row-vcenter-hcenter">
+            <p className="txt-448">Post a job</p>
+            <p className="txt-186">Home</p>
+            <p className="txt-496">Freelance</p>
+          </div>
+        </div>
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/gmwi0r3wxef-6%3A75?alt=media&token=b237ea41-9214-4aec-a8fb-424c9f11bfc0"
+          alt="Not Found"
+          className="flakes-1"
+        />
+      </div>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/gmwi0r3wxef-6%3A29?alt=media&token=06880d3d-bc62-4de5-b084-7b7421c621b1"
+        alt="Not Found"
+        className="frame-38"
+      />
+      <div className="frame-6 flex-col-hstart-vcenter">
+        <p className="txt-562">There’s a New Way to Work and get ......</p>
+        <p className="txt-517">
+          Lorem ipsum dolor sit amet Laudantium at iste velit earum tenetur
+          quaerat sed corporis ex, rem cum nesciunt non ea eligendi porro max
+        </p>
+        <div className="frame-5 flex-row-vcenter-hcenter">
+          <div className="frame-3 flex-row-vcenter-hcenter">
+            <p className="txt-673">Log In</p>
+          </div>
+          <div className="frame-4 flex-row-vcenter-hcenter">
+            <p className="txt-673">Sign Up</p>
+          </div>
+        </div>
+      </div>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/gmwi0r3wxef-6%3A22?alt=media&token=d46b8363-9d35-48b0-8852-73272628b92a"
+        alt="Not Found"
+        className="flakes-2"
+      />
+    </div>
+  )
+}
